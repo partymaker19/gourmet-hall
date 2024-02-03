5de9cea8a1af2d5186fd7b58df67b7a04e920557
Ilya <82887965+partymaker19@users.noreply.github.com>
2024-02-03 14:07:36 +0300

    feat: add adaptive

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-document.querySelectorAll(".cards__link").forEach(function (link) {
-  link.addEventListener("click", function () {
-    this.style.color = "#079c5a";
+//изменение кнопки "в корзину"
+document.querySelectorAll(".btn.btn-success.cards__btn").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    const basketTitle = event.target.parentElement.querySelector(
+      ".cards__basket-title"
+    );
+    const basketText = basketTitle.textContent;
+
+    basketTitle.textContent = "супер!";
+
+    setTimeout(() => {
+      basketTitle.textContent = basketText;
+    }, 1000);
   });
 });
 
-// document.querySelectorAll(".card").forEach(function (card) {
-//   card.addEventListener("mousemove", function (e) {
-//     let rect = card.getBoundingClientRect();
-//     let x = e.clientX - rect.left;
-//     let y = e.clientY - rect.top;
-//     card.querySelector(".card-img-top").style.transformOrigin = `${x}px ${y}px`;
-//   });
-// });
+document.querySelectorAll(".big-card__card-block").forEach(function (card) {
+  card.addEventListener("mousemove", function (e) {
+    let rect = card.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    card.querySelector(
+      ".big-card__img"
+    ).style.transformOrigin = `${x}px ${y}px`;
+  });
+});
+
+    var docWidth = document.documentElement.offsetWidth;
+    [].forEach.call(document.querySelectorAll("*"), function (el) {
+      if (el.offsetWidth > docWidth) {
+        console.log(el);
+      }
+    });
