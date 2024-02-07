c51335a663a5c467421febc0eca3f371c5d49d1e
Ilya <82887965+partymaker19@users.noreply.github.com>
2024-02-08 00:14:24 +0300

    feat: changes files

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ document.querySelectorAll(".btn.btn-success.cards__btn").forEach((button) => {
   });
 });
 
+//изменение картинки при наведении
 document.querySelectorAll(".big-card__card-block").forEach(function (card) {
   card.addEventListener("mousemove", function (e) {
     let rect = card.getBoundingClientRect();
@@ -26,9 +27,64 @@ document.querySelectorAll(".big-card__card-block").forEach(function (card) {
   });
 });
 
-    var docWidth = document.documentElement.offsetWidth;
-    [].forEach.call(document.querySelectorAll("*"), function (el) {
-      if (el.offsetWidth > docWidth) {
-        console.log(el);
+var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(document.querySelectorAll("*"), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  }
+});
+
+//изменеие цвета текста в радио
+document.addEventListener("DOMContentLoaded", function () {
+  const radioButtons = document.querySelectorAll(".radio");
+
+  // функция для обновления цвета текста
+  function updateLabelColor() {
+    document
+      .querySelectorAll(".label")
+      .forEach((label) => label.classList.remove("selected"));
+
+    const selectedRadio = document.querySelector(".radio:checked");
+    if (selectedRadio) {
+      const label = selectedRadio
+        .closest(".big-card__souse-item")
+        .querySelector(".label");
+      if (label) {
+        label.classList.add("selected");
       }
-    });
+    }
+  }
+
+  // обработчики событий на все радио кнопки
+  radioButtons.forEach((radio) => {
+    radio.addEventListener("change", updateLabelColor);
+  });
+
+  updateLabelColor();
+});
+
+//счетчик
+const incrementButton = document.querySelector(
+  ".big-card__green.big-card__max"
+);
+const decrementButton = document.querySelector(".big-card__dark.big-card__max");
+const counterDisplay = document.querySelector(".big-card__number");
+
+let counterValue = parseInt(counterDisplay.textContent, 10);
+
+function incrementCounter() {
+  if (counterValue < 25) {
+    counterValue++;
+    counterDisplay.textContent = counterValue;
+  }
+}
+
+function decrementCounter() {
+  if (counterValue > 1) {
+    counterValue--;
+    counterDisplay.textContent = counterValue;
+  }
+}
+
+incrementButton.addEventListener("click", incrementCounter);
+decrementButton.addEventListener("click", decrementCounter);
